b87fc07225258dcfdf68bdf551be069e3c995ecc
cre8tivefranklin <creative.franklinsami.gmail.com>
2019-06-17 13:40:10 -0400

    repo update

---
+++ scripts/calculator.js
@@ -0,0 +1,6 @@
+function my_calc(){
+    var input = prompt('input number');
+    console.log(input);
+}
+// use if/else statements for calculator algorithm
+// oncklick attribute for calculator keys
