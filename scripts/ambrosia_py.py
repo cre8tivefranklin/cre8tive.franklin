f7037879dbeb80f26d61267d0ee41a08d2bf15bd
Franklin Sami <franklin120889@gmail.com>
2020-11-10 16:19:14 -0500

    update repo

--- scripts/ambrosia_py.py
+++ scripts/ambrosia_py.py
@@ -1,32 +1,31 @@
+#class with food and nutrients
 
-def food_data():
-    #the following variables are arrays of different flavors textures and
-    #nutrients in food
-    #flavor[0] is the subset of 0 in the flavor array containing the words
-    #to describe acerbic flavors
-    
-    flavor =['salty', 'sweet', 'sour', 'bitter', 'umami']
-    flavor[0] = ['beets', 'celery', 'carrots', 'spinach', 'chard', 'meat']
-    
-    texture  = ['crisp','tender','soft']
-    
-    nutrients = ['sodium','potassium','magnesium','protein','sugar']
+def food():
+    class rec_alg:
+        def __init__(self, n1):
+            self.n1 = n1
+            
+    nutrients = rec_alg(['citrus','sodium','carotene','magnesium','potassium','protein','carbohydrates'])
+    print(" Here's a list of nutrients \n ")
+    for n in nutrients.n1:
+        print(n)
 
-    #some indices in the arrays above are assigned arrays of values
-    #nutrients[3] is an array of the different types of proteins that
-    #exist in food
-    nutrients[3] = ['whey','gluten','soy']
-    
+    nutri_choice = input("\n What's your choice of nutrition? \n")
+    nutrients.n1[0] = ['orange','lemon','lime \n']
+    nutrients.n1[3] = ['carrots']
+    if nutri_choice == 'citrus':
+        for n in nutrients.n1[0]:
+            print(n, " It's a very rich source of citrus which is also good for your immune system")
+            food()
+    elif nutri_choice == 'carotene':
+        for n in nutrients.n1[3]:
+            print(n)
+            food()
+    else:
+        print('Please select an option')
+        food()
+    print('Feel free to pick more options, just type food()')
+                
+        
 
-    def food_int():    
-        flav_int = input('What kind of flavor do you enjoy?\n')
-        if flav_int == 'salty':
-            print('I recommend the following veggies\n', *flavor[0], '\nEach of these are rich in sodium')
-            flav_choice = input('Whcih do think you''d enjoy most?\n')
-            if flav_choice == 'beets' or 'carrots':
-                print('Nice! \n', 'You should try making a salad out of this!')
-        elif flav_int == flavor[6]:
-                print('aromatic chickden is my recomdation')        
-    food_int()
-
-food_data()
+food()
---
+++ scripts/function.js
@@ -0,0 +1,84 @@
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+//making the canvas full screen
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+
+//english characters
+var english = "THE_NET_DIVER_DIASvElEZ_PROMETHEUSICAURS-THEDARKCORNERBEYONDTHE-GRID";
+//converting the string into an array of single characters
+english = english.split("");
+
+var font_size = 15;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+	drops[x] = 1; 
+
+//drawing the characters
+function draw()
+{
+	//Black BG for the canvas
+	//translucent BG to show trail
+	ctx.fillStyle = "rgba(255, 255, 255, .05)";
+	ctx.fillRect(0, 0, c.width, c.height);
+	
+	ctx.fillStyle = "#e60000"; //red text
+	ctx.font = font_size + "px arial";
+	//looping over drops
+	for(var i = 0; i < drops.length; i++)
+	{
+		//a random chinese character to print
+		var text = english[Math.floor(Math.random()*english.length)];
+		//x = i*font_size, y = value of drops[i]*font_size
+		ctx.fillText(text, i*font_size, drops[i]*font_size);
+		
+		//sending the drop back to the top randomly after it has crossed the screen
+		//adding a randomness to the reset to make the drops scattered on the Y axis
+		if(drops[i]*font_size > c.height && Math.random() > 0.975)
+			drops[i] = 0;
+		
+		//incrementing Y coordinate
+		drops[i]++;
+	}
+}
+i = setInterval(draw, 33);
+
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+//My own js
+
+window.onload = function() {
+  var c = document.getElementById("c");
+  var draw = c.getContext("2d");
+  var img = document.getElementById("c");
+  c.drawImage(img, 10, 10, 150, 180);
+}
